44fd55d644732f8eae2bddfd99ee2d32696a53b7
MedMehdi97 <mohamedmehdielkefif@gmail.com>
2022-01-06 11:17:57 +0100

    V1

--- src/api/Movie.js
+++ src/api/Movie.js
@@ -9,28 +9,4 @@ export async function getPeople(SearchTerm, offset) {
       console.log(`Error with function getPeople ${error.message}`);
       throw error;
     }
-    /*try {
-      let response;
-      switch (offset) {
-        case 0:
-          console.log('0');
-          response = require('../helpers/Search-start-0.json');
-          break;
-        case 20:
-          console.log('20');
-          response = require('../helpers/Search-start-20.json');
-          break;
-        case 40:
-          console.log('40');
-          response = require('../helpers/Search-start-40.json');
-          break;
-        default:
-          console.log("default");
-          response = require('../helpers/Search-start-0.json');
-      }
-      return response;
-    } catch (error) {
-      console.log(`Error with function getRestaurants ${error.message}`);
-      throw error;
-    }*/
   };
