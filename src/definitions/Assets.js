2de0fde9515372156bf7c489aab0069d50161a43
MedMehdi97 <mohamedmehdielkefif@gmail.com>
2022-01-06 11:27:41 +0100

    V1 Modif

--- src/definitions/Assets.js
+++ src/definitions/Assets.js
@@ -1,9 +1,9 @@
-import iconRate from '../../assets/assets/rate.png';
-import iconReview from '../../assets/assets/review.png';
-import iconError from '../../assets/assets/error.png';
-import iconMissingIMG from '../../assets/assets/missingImage.png';
-import iconFavEmpty from '../../assets/assets/favEmpty.png';
-import iconFavFull from '../../assets/assets/favFull.png';
+import iconRate from '../assets/assets/rate.png';
+import iconReview from '../assets/assets/review.png';
+import iconError from '../assets/assets/error.png';
+import iconMissingIMG from '../assets/assets/missingImage.png';
+import iconFavEmpty from '../assets/assets/favEmpty.png';
+import iconFavFull from '../assets/assets/favFull.png';
 
 const Assets = {
   icons: {
